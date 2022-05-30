5b3f2c175e18a9d1b6ab70c6383d9a4c9f75d10d
blckclov3r <blckclov3r@gmail.com>
2022-05-31 04:17:26 +0800

    utilize react-query ClientProvider

--- src/components/RickMorty/Character.js
+++ src/components/RickMorty/Character.js
@@ -1,26 +1,28 @@
 
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useQuery } from 'react-query';
 
+const KEYS = {
+    CHARACTERS: "characters"
+}
 
 export default function Character() {
 
-    const [characters, setCharacters] = useState(null);
-
     const fetchCharactes = async () => {
         const response = await fetch("https://rickandmortyapi.com/api/character");
-        const data = await response.json();
-        setCharacters(data);
+        return response.json();
     }
-
-    useEffect(() => {
-        fetchCharactes();
-    }, [])
-
+    const {data, status} = useQuery(KEYS.CHARACTERS, fetchCharactes);
+    if(status === "loading"){
+        return <div>Loading ... </div>
+    }
+    if(status === "error"){
+        return <div>Error... </div>
+    }
     return (
-
         <div>
-            {characters && characters.results.map(item => (
+            {data && data.results.map(item => (
                 <div key={item.name}>{item.name}</div>
             ))}
         </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import {QueryClientProvider, QueryClient} from 'react-query'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient(QueryClientProvider);
 root.render(
-    <App />
+    <QueryClientProvider client={queryClient}>
+         <App />
+    </QueryClientProvider>
+
 );
 
