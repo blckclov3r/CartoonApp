32dab5a87ef5be8e4def46a6bbb5d454d1f5e461
blckclov3r <blckclov3r@gmail.com>
2022-06-03 01:31:49 +0800

    strict mode

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import {QueryClientProvider, QueryClient} from 'react-query'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient(QueryClientProvider);
 root.render(
-    <QueryClientProvider client={queryClient}>
-         <App />
-    </QueryClientProvider>
-
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>
 );
 
