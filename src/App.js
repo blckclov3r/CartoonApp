4d95088618cfccc48f5b9a1a3048f21492f96450
blckclov3r <blckclov3r@gmail.com>
2022-06-03 01:57:52 +0800

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import NavHeader from './components/NavHeader';
-import {HashRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import RickMorty from './components/RickMorty/Rick&Morty';
 import Footer from './components/Footer';
 import Simpson from './components/SImpson/Simpson';
 import Cartoon from './components/Futarama/Futarama';
-import NotFound from './components/NotFound';
+
 
 function App() {
   return (
-     <HashRouter>
+     <BrowserRouter >
       <div className="App">
           <NavHeader />
           <div className='main'>
-              <Routes>
+              <Routes  >
                   <Route exact path="/" element={<Home />} />
                   <Route path="/Futarama" element={<Cartoon />} />
                   <Route path="/Rick&Morty" element={<RickMorty />} />
@@ -24,7 +24,7 @@ function App() {
           </div>
           <Footer />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
