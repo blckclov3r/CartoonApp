85f3c1155c6a6d72befd1064411bd8672a10a391
blckclov3r <blckclov3r@gmail.com>
2022-06-03 15:54:01 +0800

    deploy

--- src/components/SImpson/CardView.js
+++ src/components/SImpson/CardView.js
@@ -7,7 +7,7 @@ export default function CardView({character, characterDirection, image, quote}) 
   return (
     <Col className='my-2'>
          <Card style={{ width: '18rem'}} className="h-100 shadow-sm">
-            <Card.Img variant="top" src={image} className="img-fluid img-thumbnail"  style={{height: "300px", width: "300px"}} onError={(img)=>{
+            <Card.Img variant="top" src={image} className="img-fluid p-3"  style={{height: "300px", width: "300px"}} onError={(img)=>{
             img.target.src = "https://theinsidetrackinc.com/wp-content/uploads/2019/11/square-placeholder.jpg"
           }}  />
             <Card.Header className='d-flex align-items-center'>
